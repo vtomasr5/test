a9a245fb6352875cd36ac831bac929c296415301
Vicenç Juan Tomàs Montserrat <vtomasr5@gmail.com>
2021-01-30 22:53:01 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,16 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-resource "null_resource" "this" {
+resource "null_resource" "null1" {
 }
 
-output "null_id" {
-  value = null_resource.this.id
+resource "null_resource" "null2" {
+}
+
+output "null1_id" {
+  value = null_resource.null1.id
+}
+
+output "null2_id" {
+  value = null_resource.null2.id
 }
